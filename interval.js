f448e37812e918e51ef0163c8f4013b9aeaf8ed5
Stepan82-st <bryl.step@tut.by>
2021-04-09 23:49:31 +0300

    Update interval.js

--- interval.js
+++ interval.js
@@ -1,40 +1,38 @@
-var columns = ['C','C#','Db','D','D#','Eb','E','F','F#','Gb','G','G#','Ab','A','A#','Bb','B','B#','Cb','C'];
-var intervals = {m2:2, M2:3, m3:3, M3:4, P4:7, P5:13, m6:8, M6:9, m7:10, M7:18, P8:20}
-var nup = ['asc', 'dsc'];
-console.log(intervalConstruction("m2","C","asc"));
+const columns = ['C','C#','Db','D','D#','Eb','E','F','F#','Gb','G','G#','Ab','A','A#','Bb','B','B#','Cb','C'];
+const intervals = {m2:2, M2:3, m3:3, M3:4, P4:7, P5:13, m6:8, M6:9, m7:10, M7:18, P8:20}
+const nup = ['asc', 'dsc'];
+function intervalConstruction(arr){
+const interval = arr[0];
+const column = arr[1];
+const rightDirection = arr[2] === 'asc';
+const columnIndex = columns.indexOf(column);
 
-function intervalConstruction( interval, column, nup ){
-var columnIndex = columns.indexOf(column);
-    var intervalValue = intervals[interval];
-   if( nup === "asc" )
-   {
-    var calculatedIndex = columnIndex + intervalValue;
-   }
-   if( nup === "dsc" )
-   {
-   	 var calculatedIndex = columnIndex - intervalValue;
-   }
-   else 
-   	{
-   		var calculatedIndex = columnIndex + intervalValue 
-   	}
-return columns[calculatedIndex % columns.length];
+if (rightDirection) {
+return columns[columnIndex + intervals[interval]];
 }
-var arr = intervalIdentification ('C', 'Db');
-function  intervalIdentification ( column1, column2 )  {
-	var columnIndex1 = columns.indexOf(column1);
-    var columnIndex2 = columns.indexOf(column2);
-    var intervalIdenf = columnIndex2 - columnIndex1;
-   
-    return intervalIdenf;
+else {
+return columns[columnIndex - intervals[interval]];
 }
-function getKeyByValue(intervalIdenf, value) {
-            for (var prop in intervalIdenf) {
-                if (intervalIdenf.hasOwnProperty(prop)) {
-                    if (intervalIdenf[prop] === value)
-                    return prop;
-                }
-            }
-      }
-        ans = getKeyByValue(intervals, arr);
-        console.log(ans);
+}
+
+console.log('result', intervalConstruction(['P4', 'G#', 'dsc']));
+
+
+getKeyByValue = (obj, value) => {
+let key;
+for (let prop in obj) {
+if (obj.hasOwnProperty(prop) && obj[prop] === value) {
+key = prop;
+}
+}
+return key;
+}
+
+intervalIdentification = (arr) => {
+const firstColumnIndex = columns.indexOf(arr[0]);
+const secondColumnIndex = columns.indexOf(arr[1]);
+const rightDirection = arr[2] !== 'dsc';
+const interval = rightDirection ? secondColumnIndex - firstColumnIndex : firstColumnIndex - secondColumnIndex;
+return getKeyByValue(intervals, interval);
+}
+     console.log ('result', intervalIdentification(['G#', 'D#', 'dsc']))
